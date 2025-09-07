720a440f5f19517ddc1e37cea775495e87ae991d
christian-gamo <christian.gamo.pro@gmail.com>
2025-09-07 20:12:33 +0200

    Address model attributes

--- src/main/java/com/cpg/pprojects/ecommerce/model/User.java
+++ src/main/java/com/cpg/pprojects/ecommerce/model/User.java
@@ -15,9 +15,8 @@ public class User {
     private Cart cart;
     private Set<Product> products;
 
-    public User(){
+    public User(){}
 
-    }
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
