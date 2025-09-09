62474701d3813e874a6007c7a5c58ebc1376c0e1
christian-gamo <christian.gamo.pro@gmail.com>
2025-09-09 17:54:36 +0200

    CartItem model attributes and g/s etters

--- src/main/java/com/cpg/pprojects/ecommerce/model/CartItem.java
+++ src/main/java/com/cpg/pprojects/ecommerce/model/CartItem.java
@@ -1,4 +1,59 @@
 package com.cpg.pprojects.ecommerce.model;
 
 public class CartItem {
+    private long idCartItem;
+    private int quantity;
+    private double discount;
+    private double productPrice;
+
+    private Cart cart;
+    private Product product;
+
+    public long getIdCartItem() {
+        return idCartItem;
+    }
+
+    public void setIdCartItem(long idCartItem) {
+        this.idCartItem = idCartItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
