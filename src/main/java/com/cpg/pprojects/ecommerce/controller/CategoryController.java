5dbb40bf5bd3a51ebe8de47687b2e1916d94e7ce
christian-gamo <christian.gamo.pro@gmail.com>
2025-09-20 01:32:23 +0200

    Removed category early project drafts, to be modified

--- src/main/java/com/cpg/pprojects/ecommerce/controller/CategoryController.java
+++ src/main/java/com/cpg/pprojects/ecommerce/controller/CategoryController.java
@@ -1,28 +1,8 @@
 package com.cpg.pprojects.ecommerce.controller;
 
-import com.cpg.pprojects.ecommerce.entity.category.model.Category;
-import com.cpg.pprojects.ecommerce.service.impls.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CategoryController {
-    private CategoryService categoryService;
 
-    public CategoryController() { }
-
-    @GetMapping("/api/public/categories")
-    public List<Category> getCategories() {
-        return categoryService.getCategories();
-    }
-
-    @PostMapping("/api/public/categories")
-    public String addCategory(@RequestBody Category category) {
-        categoryService.addCategory(category);
-        return String.format("Category %s added successfully", category.getCategoryName());
-    }
 }
