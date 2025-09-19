e73b68cd4401db4c13a52eafd287dfd8e1f99323
christian-gamo <christian.gamo.pro@gmail.com>
2025-09-20 01:39:24 +0200

    common ResourceNotFoundException created

---
+++ src/main/java/com/cpg/pprojects/ecommerce/usecase/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,26 @@
+package com.cpg.pprojects.ecommerce.usecase.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    String resourceName;
+    String field;
+    String fieldName;
+    Long idField;
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String resourceName, String field, String fieldName) {
+        super(String.format("%s not found with %s: %s", resourceName, field, fieldName));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldName = fieldName;
+    }
+
+    public ResourceNotFoundException(String resourceName, String field, Long idField) {
+        super(String.format("%s not found with %s: %d", resourceName, field, idField));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.idField = idField;
+    }
+}
+
