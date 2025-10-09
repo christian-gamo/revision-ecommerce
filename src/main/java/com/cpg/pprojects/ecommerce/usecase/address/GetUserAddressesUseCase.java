0066fa5066d5963c097e6554df3e477ee96a4d69
christian-gamo <christian.gamo.pro@gmail.com>
2025-10-09 19:31:32 +0200

    GetUserAddressesUseCase done

--- src/main/java/com/cpg/pprojects/ecommerce/usecase/address/GetUserAddressesUseCase.java
+++ src/main/java/com/cpg/pprojects/ecommerce/usecase/address/GetUserAddressesUseCase.java
@@ -2,8 +2,12 @@ package com.cpg.pprojects.ecommerce.usecase.address;
 
 import com.cpg.pprojects.ecommerce.domain.address.model.Address;
 import com.cpg.pprojects.ecommerce.domain.address.repository.IAddressRepository;
+import com.cpg.pprojects.ecommerce.domain.user.model.User;
+import com.cpg.pprojects.ecommerce.usecase.address.dto.AddressDTO;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GetUserAddressesUseCase {
     private final IAddressRepository addressRepository;
@@ -12,9 +16,14 @@ public class GetUserAddressesUseCase {
         this.addressRepository = addressRepository;
     }
 
-    public List<Address> execute(Long idUser){
-        return addressRepository.findByIdUser(idUser);
+    public List<AddressDTO> execute(User user){
+        if (user == null || user.getAddresses() == null) {
+            return Collections.emptyList();
+        }
+        List<Address> addresses = user.getAddresses();
+        return addresses.stream()
+                .map(AddressDTO::new)
+                .collect(Collectors.toList());
     }
-
 
 }
