9ced54318dba80a0b3782809cc541114e22a8bea
christian-gamo <christian.gamo.pro@gmail.com>
2025-10-22 00:40:24 +0200

    forgotten GetUserAddressesUseCaseController @RestController

--- src/main/java/com/cpg/pprojects/ecommerce/infrastructure/address/controller/GetUserAddressesUseCaseController.java
+++ src/main/java/com/cpg/pprojects/ecommerce/infrastructure/address/controller/GetUserAddressesUseCaseController.java
@@ -7,9 +7,11 @@ import com.cpg.pprojects.ecommerce.usecase.address.dto.AddressDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class GetUserAddressesUseCaseController {
     private final GetUserAddressesUseCase getUserAddressesUseCase;
     private final AuthUtil authUtil;
