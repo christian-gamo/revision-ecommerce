ac22dd5702789a2dcd5016069692bb6e63e214f7
christian-gamo <christian.gamo.pro@gmail.com>
2025-10-22 00:27:41 +0200

    GetAddressesUseCase + controller

---
+++ src/main/java/com/cpg/pprojects/ecommerce/infrastructure/address/controller/GetAddressesUseCaseController.java
@@ -0,0 +1,26 @@
+package com.cpg.pprojects.ecommerce.infrastructure.address.controller;
+
+import com.cpg.pprojects.ecommerce.usecase.address.GetAddressesUseCase;
+import com.cpg.pprojects.ecommerce.usecase.address.dto.AddressDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GetAddressesUseCaseController {
+    private final GetAddressesUseCase getAddressesUseCase;
+
+    public GetAddressesUseCaseController(GetAddressesUseCase getAddressesUseCase) {
+        this.getAddressesUseCase = getAddressesUseCase;
+    }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<AddressDTO>> getAddresses(){
+        List<AddressDTO> addressList = getAddressesUseCase.getAddresses();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/cpg/pprojects/ecommerce/usecase/address/GetAddressesUseCase.java
@@ -0,0 +1,27 @@
+package com.cpg.pprojects.ecommerce.usecase.address;
+
+import com.cpg.pprojects.ecommerce.domain.address.model.Address;
+import com.cpg.pprojects.ecommerce.domain.address.repository.IAddressRepository;
+import com.cpg.pprojects.ecommerce.usecase.address.dto.AddressDTO;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GetAddressesUseCase {
+    private final IAddressRepository addressRepository;
+
+    public GetAddressesUseCase(IAddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public List<AddressDTO> getAddresses(){
+        List<Address> addresses = addressRepository.findAll();
+        if (addresses.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return addresses.stream()
+                .map(AddressDTO::new)
+                .collect(Collectors.toList());
+    }
+}
