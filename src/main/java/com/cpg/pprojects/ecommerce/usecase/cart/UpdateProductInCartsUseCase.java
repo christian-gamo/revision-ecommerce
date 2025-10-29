5cc57742ff496909e0512af57cfb677a61deecf0
christian-gamo <christian.gamo.pro@gmail.com>
2025-10-29 15:15:37 +0100

    UpdateProductsInCartsUseCase created

---
+++ src/main/java/com/cpg/pprojects/ecommerce/usecase/cart/UpdateProductInCartsUseCase.java
@@ -0,0 +1,50 @@
+package com.cpg.pprojects.ecommerce.usecase.cart;
+
+import com.cpg.pprojects.ecommerce.domain.cart.model.Cart;
+import com.cpg.pprojects.ecommerce.domain.cart.repository.ICartRepository;
+import com.cpg.pprojects.ecommerce.domain.cartItem.model.CartItem;
+import com.cpg.pprojects.ecommerce.domain.cartItem.repository.ICartItemRepository;
+import com.cpg.pprojects.ecommerce.domain.product.model.Product;
+import com.cpg.pprojects.ecommerce.domain.product.repository.IProductRepository;
+import com.cpg.pprojects.ecommerce.usecase.exceptions.APIException;
+import com.cpg.pprojects.ecommerce.usecase.exceptions.ResourceNotFoundException;
+
+public class UpdateProductInCartsUseCase {
+    private final ICartRepository cartRepository;
+    private final IProductRepository productRepository;
+    private final ICartItemRepository cartItemRepository;
+
+    public UpdateProductInCartsUseCase(
+            ICartRepository cartRepository,
+            IProductRepository productRepository,
+            ICartItemRepository cartItemRepository
+    ) {
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+        this.cartItemRepository = cartItemRepository;
+    }
+
+    public void execute (Long idCart, Long idProduct){
+        Cart cart = cartRepository.findById(idCart)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart", "idCart", idCart));
+
+        Product product = productRepository.findById(idProduct)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "idProduct", idProduct));
+
+        CartItem cartItem = cartItemRepository.findCartItemByIdCartAndIdProduct(idCart, idProduct);
+
+        if (cartItem == null) {
+            throw new APIException("Product " + product.getProductName() + " not available in the cart!!!");
+        }
+
+        double cartPrice = cart.getTotalPrice()
+                - (cartItem.getProductPrice() * cartItem.getQuantity());
+
+        cartItem.setProductPrice(product.getSpecialPrice());
+
+        cart.setTotalPrice(cartPrice
+                + (cartItem.getProductPrice() * cartItem.getQuantity()));
+
+        cartItemRepository.save(cartItem);
+    }
+}
